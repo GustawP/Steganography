23ed50f0721dad6d8200f07c65984ef4aa226220
GustawP <gutek2003@gmail.com>
2024-11-02 23:26:01 +0100

    deleted comments and added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Steganography
+
+### A program made to encrypt and decrypt text into/from image. Works with .png and .bmp files
+
+# Install
+
+Make a copy of a whole repository on your local device and reload CMake
+
+# Requirements
+
+Works fine on Win10/11
--- main.cpp
+++ main.cpp
@@ -9,21 +9,21 @@
 #include <format>
 #include <chrono>
 
-// Program stworzony przez: GUSTAW PLUTA, na potrzeby projektu na studia dnia 13.06.2024 r.
-// Steganografia obrazowa
+// Program created by GUSTAW PLUTA for a uni project
+// Steganography
 
-// -e C:\Users\gutek\Desktop\zdjecie.bmp "Sekretna wiadomosc .bmp abc123!"
-// -e C:\Users\gutek\Desktop\zdjecie.png "Wiadomosc sekretna .png abc123?"
-// -e C:\Users\gutek\Desktop\zdjecie.bmp "ęąśćż"
+// -e X:\Path\to\photo\photo.bmp "Sekretna wiadomosc .bmp abc123!"
+// -e X:\Path\to\photo\photo.png "Wiadomosc sekretna .png abc123?"
+// -e X:\Path\to\photo\photo.bmp "ęąśćż"
 
-// -d C:\Users\gutek\Desktop\zdjecie.bmp 31
-// -d C:\Users\gutek\Desktop\zdjecie.png 31
+// -d X:\Path\to\photo\photo.bmp 31
+// -d X:\Path\to\photo\photo.png 31
 
-// -c C:\Users\gutek\Desktop\zdjecie.bmp "Czy sie zmiesci to"
-// -c C:\Users\gutek\Desktop\zdjecie.png "Czy to sie zmiesci"
+// -c X:\Path\to\photo\photo.bmp "Czy sie zmiesci to"
+// -c X:\Path\to\photo\photo.png "Czy to sie zmiesci"
 
-// -i C:\Users\gutek\Desktop\zdjecie.bmp
-// -i C:\Users\gutek\Desktop\zdjecie.png
+// -i X:\Path\to\photo\photo.bmp
+// -i X:\Path\to\photo\photo.png
 
 auto generate_random_numbers_from_password(const std::string &password, int count) -> std::vector<int>;
 auto embed_message_in_image(sf::Image &image, const std::string &password, const std::string &message) -> void;
@@ -32,7 +32,6 @@ auto print_file_info(const std::string &file_path) -> void;
 auto check_embedding_possibility(const std::string &file_path, const std::string &message) -> void;
 
 auto main(int argc, char *argv[]) -> int {
-    // Sprawdzenie, czy podano wystarczajaca liczbe argumentow
     if (argc < 2) {
         fmt::print("Brak argumentow. Uzyj -h lub --help, aby wyswietlic pomoc.\n");
         return -1;
@@ -40,7 +39,6 @@ auto main(int argc, char *argv[]) -> int {
 
     std::string flag = argv[1];
 
-    // Obsluga flagi -h lub --help
     if (flag == "-h" || flag == "--help") {
         fmt::print("Uzycie:\n");
         fmt::print("  -i, --info     <sciezka do pliku>                        Wyswietla informacje o pliku\n");
@@ -51,7 +49,6 @@ auto main(int argc, char *argv[]) -> int {
         return 0;
     }
 
-    // Obsluga flagi -i lub --info
     if (flag == "-i" || flag == "--info") {
         if (argc != 3) {
             fmt::print("Nieprawidlowe uzycie flagi -i. Uzyj -h lub --help, aby uzyskac pomoc.\n");
@@ -62,7 +59,6 @@ auto main(int argc, char *argv[]) -> int {
         return 0;
     }
 
-    // Obsluga flagi -e lub --encrypt
     if (flag == "-e" || flag == "--encrypt") {
         if (argc != 4) {
             fmt::print("Nieprawidlowe uzycie flagi -e. Uzyj -h lub --help, aby uzyskac pomoc.\n");
@@ -91,7 +87,6 @@ auto main(int argc, char *argv[]) -> int {
         return 0;
     }
 
-    // Obsluga flagi -d lub --decrypt
     if (flag == "-d" || flag == "--decrypt") {
         if (argc != 4) {
             fmt::print("Nieprawidlowe uzycie flagi -d. Uzyj -h lub --help, aby uzyskac pomoc.\n");
@@ -115,7 +110,6 @@ auto main(int argc, char *argv[]) -> int {
         return 0;
     }
 
-    // Obsluga flagi -c lub --check
     if (flag == "-c" || flag == "--check") {
         if (argc != 4) {
             fmt::print("Nieprawidlowe uzycie flagi -c. Uzyj -h lub --help, aby uzyskac pomoc.\n");
@@ -127,7 +121,6 @@ auto main(int argc, char *argv[]) -> int {
         return 0;
     }
 
-    // Nieznana flaga
     fmt::print("Nieznana flaga. Uzyj -h lub --help, aby uzyskac pomoc.\n");
     return -1;
 }
@@ -149,7 +142,6 @@ auto embed_message_in_image(sf::Image &image, const std::string &password, const
     auto height = image.getSize().y;
     auto pixel_count = width * height;
 
-    // Konwersja wiadomosci na postac binarna
     std::vector<bool> binary_message;
     for (char c: message) {
         std::bitset<8> bits(c);
@@ -158,16 +150,14 @@ auto embed_message_in_image(sf::Image &image, const std::string &password, const
         }
     }
 
-    // Generowanie losowych pozycji do osadzenia wiadomosci
     auto random_numbers = generate_random_numbers_from_password(password, binary_message.size());
 
-    // Osadzanie bitow wiadomosci w LSB kanalu czerwonego
     for (size_t i = 0; i < binary_message.size(); ++i) {
         auto pos = random_numbers[i] % pixel_count;
         auto x = pos % width;
         auto y = pos / width;
         sf::Color color = image.getPixel(x, y);
-        color.r = (color.r & 0xFE) | binary_message[i]; // Zastapienie LSB bitem wiadomosci
+        color.r = (color.r & 0xFE) | binary_message[i];
         image.setPixel(x, y, color);
     }
 }
@@ -177,23 +167,19 @@ auto decode_message_from_image(const sf::Image &image, const std::string &passwo
     auto height = image.getSize().y;
     auto pixel_count = width * height;
 
-    // Obliczanie liczby bitow w wiadomosci
     auto num_bits = message_length * 8;
 
-    // Generowanie losowych pozycji do odczytania wiadomosci
     auto random_numbers = generate_random_numbers_from_password(password, num_bits);
 
-    // Wyciaganie binarnej wiadomosci z obrazu
     std::vector<bool> binary_message(num_bits);
     for (auto i = 0; i < num_bits; ++i) {
         auto pos = random_numbers[i] % pixel_count;
         auto x = pos % width;
         auto y = pos / width;
         sf::Color color = image.getPixel(x, y);
-        binary_message[i] = color.r & 1; // Wyciaganie LSB
+        binary_message[i] = color.r & 1;
     }
 
-    // Konwersja binarnej wiadomosci na tekst
     std::string message;
     for (auto i = 0; i < num_bits; i += 8) {
         std::bitset<8> bits;
@@ -206,7 +192,6 @@ auto decode_message_from_image(const sf::Image &image, const std::string &passwo
 }
 
 auto print_file_info(const std::string &file_path) -> void {
-    // Sprawdzenie, czy plik istnieje
     if (!std::filesystem::exists(file_path)) {
         fmt::print("Plik nie istnieje.\n");
         return;
@@ -214,19 +199,16 @@ auto print_file_info(const std::string &file_path) -> void {
 
     std::filesystem::path path(file_path);
 
-    // Wyswietlanie podstawowych informacji o pliku
     fmt::print("Informacje o pliku:\n");
     fmt::print("Sciezka: {}\n", path.string());
     fmt::print("Rozmiar: {} bajtow\n", std::filesystem::file_size(file_path));
     std::filesystem::file_time_type f_time = std::filesystem::last_write_time(file_path);
     std::cout << std::format("File write time is {}\n", f_time);
 
-    // Sprawdzenie, czy plik jest obslugiwanym formatem (dla przykladu .png i .jpg)
     auto extension = path.extension().string();
     if (extension == ".png" || extension == ".jpg") {
         fmt::print("Obslugiwany format pliku: {}\n", extension);
 
-        // Dodatkowe informacje dla plikow graficznych
         sf::Image image;
         if (image.loadFromFile(file_path)) {
             fmt::print("Wymiary obrazu: {}x{}\n", image.getSize().x, image.getSize().y);
@@ -239,7 +221,6 @@ auto print_file_info(const std::string &file_path) -> void {
 }
 
 auto check_embedding_possibility(const std::string &file_path, const std::string &message) -> void {
-    // Zaladowanie obrazu
     sf::Image image;
     if (!image.loadFromFile(file_path)) {
         fmt::print("Nie udalo sie zaladowac obrazu\n");
@@ -250,7 +231,6 @@ auto check_embedding_possibility(const std::string &file_path, const std::string
     auto height = image.getSize().y;
     auto pixel_count = width * height;
 
-    // Konwersja wiadomosci na postac binarna
     std::vector<bool> binary_message;
     for (auto c : message) {
         std::bitset<8> bits(c);
@@ -259,7 +239,6 @@ auto check_embedding_possibility(const std::string &file_path, const std::string
         }
     }
 
-    // Sprawdzenie, czy wiadomosc zmiesci sie w obrazie
     if (binary_message.size() > pixel_count) {
         fmt::print("Wiadomosc jest zbyt dluga, aby zapisac ja w podanym obrazie.\n");
     } else {
